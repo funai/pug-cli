6b7ef2fdcbb594e01c3d6a4fa61d0f273f29ea48
Jun FUNAI <jfunai@intrix.co.jp>
2020-09-23 10:52:22 +0900

    include directory should be specified with `--obj` option (json config).

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ function renderFile(path, rootPath) {
     let page = matter.read(path);
     options.filename = page.path;
     if(page.data.layout){
-      page.content = `extends _layouts/${page.data.layout}
+      page.content = `extends ${options.includes}/${page.data.layout}
 ${page.content}`;
     }
     options.page = page;
